e3867a12b8fa523874c51711574416c9e47f8a35
Kristina Grebneva <kristina.o.grebneva@gmail.com>
2024-01-30 19:29:33 +0100

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Kristina Grebneva on 30.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
